0ec27bd9d7a61bb323214fe918c8444b74548109
VishalRashmika <vishal.rashmika.perera@gmail.com>
2024-11-30 20:21:56 +0530

    updates

--- server.py
+++ server.py
@@ -1,6 +1,53 @@
 from flask import Flask, request, jsonify, send_file
-import utils
 import datetime
+from PIL import Image, ImageDraw, ImageFont
+import sys
+import os
+
+def generate_answer_image(input_text, output_name):
+
+    bg_image_width = 697
+    bg_image_height = 135
+    font_size = 85
+    font_path = "./src/font.ttf"
+    text = input_text
+    output_file = output_name
+    color = (236,75,104)
+    bg_img = "./src/bg.png"
+
+
+    if len(text) > 10:
+        raise ValueError("Text exceeds the maximum limit of 10 characters.")
+
+    image = Image.open(bg_img)
+    draw = ImageDraw.Draw(image)
+
+    try:
+        font = ImageFont.truetype(font_path, font_size)
+    except IOError:
+        print("ERROR FONT FILE FOUND")
+        sys.exit()
+
+    text_width, text_height = draw.textsize(text, font=font)
+    x_position = (bg_image_width - text_width) // 2
+    y_position = (bg_image_height - text_height) // 2
+    draw.text((x_position, y_position), text, color, font=font)
+
+    # Save the image
+    image.save(output_file)
+    print(f"Image saved as {output_file}")
+
+
+def clean_generations(directory_path):
+    try:
+        for filename in os.listdir(directory_path):
+            if filename.endswith('.png'):
+                os.remove(os.path.join(directory_path, filename))
+        return "All files deleted successfully."
+    except:
+        return "Error occurred while deleting files."
+    
+
 
 app = Flask(__name__)
 
@@ -10,13 +57,13 @@ def generate():
     image_name = f"{str(ct).replace(' ','-').replace('.','-')}.png"
     output_image = f"./exports/{image_name}"
     text = request.args.get('text')
-    utils.generate_answer_image(text, output_image)
+    generate_answer_image(text, output_image)
 
     return send_file(output_image, mimetype='image/png')
 
 @app.route('/clean', methods=['GET'])
 def clean():
-    out = utils.clean_generations('./exports')
+    out = clean_generations('./exports')
     status = {
         'status' : out
     }
